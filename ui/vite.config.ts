c570f4d9d798eae8b27142b76bcdf2bf09f97b00
AlAsadNurRiyad <asadnurriyad@appscode.com>
2022-06-22 14:47:23 +0600

    setup ui-builder to load from the yaml file
    Signed-off-by: AlAsadNurRiyad <asadnurriyad@appscode.com>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,11 +1,12 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import pluginYaml from "vite-plugin-yaml2";
 
 import { join } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), pluginYaml()],
   resolve: {
     alias: {
       "@": join(__dirname, "/src"),
